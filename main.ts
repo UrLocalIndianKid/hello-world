ce37019c41aff6b851320b3b33651c4a0567b715
UrLocalIndianKid <122555211+UrLocalIndianKid@users.noreply.github.com>
2023-01-13 12:15:02 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,3 @@
 input.onButtonPressed(Button.A, function () {
-	
+    music.playMelody("C D E F G A B C5 ", 120)
 })
-basic.showLeds(`
-    # # # # #
-    # # # # #
-    # # # # #
-    # # # # #
-    # # # # #
-    `)
