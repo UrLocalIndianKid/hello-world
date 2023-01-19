d6813cbcd5080e00f7a5202ff391afac343c8fd6
UrLocalIndianKid <122555211+UrLocalIndianKid@users.noreply.github.com>
2023-01-19 12:34:29 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,19 @@
 input.onButtonPressed(Button.A, function () {
-    music.ringTone(10000)
-    music.setVolume(25599)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    music.playMelody("C D E F G A B C5 ", 226)
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("GET ANNOYED")
+    music.setVolume(255)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 5000, 255, 211, 9999, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("A + B")
+})
+basic.showString("PRESS A")
